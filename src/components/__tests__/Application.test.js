78bae1c6abdcf21a23307a40bd5f898b597bcfa2
Pihu Gupta <pihugupta0501@gmail.com>
2024-03-30 15:29:16 -0700

    Add a new integration test to book or cancel an appointment

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -7,6 +7,8 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
+
+
 it("defaults to Monday and changes the schedule when a new day is selected", () => {
   const { queryByText, findByText } = render(<Application />);
 
@@ -14,4 +16,9 @@ it("defaults to Monday and changes the schedule when a new day is selected", () 
     fireEvent.click(queryByText("Tuesday"));
     expect(queryByText("Leopold Silvers")).toBeInTheDocument();
   });
+});
+
+
+it("loads data, books an interview and reduces the spots remaining for the first day by 1", () => {
+
 });
