e04e266548d717e25ca4b0f64e273355c3d16cbd
Pihu Gupta <pihugupta0501@gmail.com>
2024-03-30 15:30:46 -0700

    Add a descrie block for the Application tests

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -7,18 +7,21 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
+describe("Application", () => {
 
-
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { queryByText, findByText } = render(<Application />);
-
-  return findByText("Monday").then(() => {
-    fireEvent.click(queryByText("Tuesday"));
-    expect(queryByText("Leopold Silvers")).toBeInTheDocument();
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { queryByText, findByText } = render(<Application />);
+  
+    return findByText("Monday").then(() => {
+      fireEvent.click(queryByText("Tuesday"));
+      expect(queryByText("Leopold Silvers")).toBeInTheDocument();
+    });
   });
-});
+  
+  
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", () => {
+  
+  });
 
+})
 
-it("loads data, books an interview and reduces the spots remaining for the first day by 1", () => {
-
-});
