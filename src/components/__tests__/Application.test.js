a79ac68718a992b2bb9068a550b51eabdba74cb8
Pihu Gupta <pihugupta0501@gmail.com>
2024-03-30 14:26:13 -0700

    Add the part of test where the application changes the schedule when a new day is selected, in this case using the predefined mock data in the fixtures object

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,15 +1,17 @@
 import React from "react";
 
 import { render, cleanup } from "@testing-library/react";
-import { act } from 'react-dom/test-utils';
+import { fireEvent } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
 it("defaults to Monday and changes the schedule when a new day is selected", () => {
-   
-  const { findByText } = render(<Application />);
-  findByText("Monday");
-  
+  const { queryByText, findByText } = render(<Application />);
+
+  return findByText("Monday").then(() => {
+    fireEvent.click(queryByText("Tuesday"));
+    expect(queryByText("Leopold Silvers")).toBeInTheDocument();
+  });
 });
